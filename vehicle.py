db1ebe64bde2fde6a72af267f337e5ae283f6fc3
rriesebos <rriesebos@hotmail.com>
2020-10-08 20:44:56 +0200

    Refactored VehicleParameters, added AggressiveCar and CarefulCar

--- vehicle.py
+++ vehicle.py
@@ -1,3 +1,18 @@
+from collections import namedtuple
+
+
+"""
+VehicleParameters represent a list of parameters related to a vehicle:
+    length: Length of a vehicle [m]
+    desired_velocity: Desired velocity of a vehicle [m/s]
+    desired_time_headway: Desired time gap between the car and the next vehicle [s]
+    max_acceleration:  Maximum acceleration [m/s^2]
+    comfortable_deceleration: Comfortable deceleration (braking) [m/s^2]
+"""
+VehicleParameters = namedtuple('VehicleParameters', ['length', 'desired_velocity', 'desired_time_headway',
+                                                     'max_acceleration', 'comfortable_deceleration'])
+
+
 class Vehicle:
     """
     Args:
@@ -8,7 +23,7 @@ class Vehicle:
                             comfortable_deceleration parameters of the vehicle in the given order
         next_vehicle: next vehicle on the road, the vehicle in front of this vehicle
     """
-    def __init__(self, position, velocity, traffic_model, vehicle_parameters, next_vehicle=None):
+    def __init__(self, position, velocity, traffic_model, vehicle_parameters: VehicleParameters, next_vehicle=None):
         self.position = position
         self.velocity = velocity
         self.acceleration = 0
@@ -47,50 +62,54 @@ class Vehicle:
 
 
 class Car(Vehicle):
-    CAR_LENGTH = 5
-
-    # Desired velocity of a car [m/s]
-    DESIRED_VELOCITY = 100 / 3.6
-
-    # Desired time gap between the car and the next vehicle [s]
-    DESIRED_TIME_HEADWAY = 1.5
-
-    # Maximum acceleration [m/s^2]
-    MAX_ACCELERATION = 0.3
-
-    # Comfortable deceleration (braking) [m/s^2]
-    COMFORTABLE_DECELERATION = 3.0
-
-    CAR_PARAMETERS = (CAR_LENGTH,
-                      DESIRED_VELOCITY,
-                      DESIRED_TIME_HEADWAY,
-                      MAX_ACCELERATION,
-                      COMFORTABLE_DECELERATION)
+    CAR_PARAMETERS = VehicleParameters(
+        length=5,
+        desired_velocity=100 / 3.6,
+        desired_time_headway=1.5,
+        max_acceleration=0.3,
+        comfortable_deceleration=3.0
+    )
 
     def __init__(self, position, velocity, traffic_model, next_vehicle=None):
         super().__init__(position, velocity, traffic_model, self.CAR_PARAMETERS, next_vehicle)
 
 
 class Truck(Vehicle):
-    TRUCK_LENGTH = 19
-
-    # Desired velocity of a truck [m/s]
-    DESIRED_VELOCITY = 80 / 3.6
-
-    # Desired time gap between the truck and the next vehicle [s]
-    DESIRED_TIME_HEADWAY = 1.7
-
-    # Maximum acceleration [m/s^2]
-    MAX_ACCELERATION = 0.3
-
-    # Comfortable deceleration (braking) [m/s^2]
-    COMFORTABLE_DECELERATION = 2.0
-
-    TRUCK_PARAMETERS = (TRUCK_LENGTH,
-                        DESIRED_VELOCITY,
-                        DESIRED_TIME_HEADWAY,
-                        MAX_ACCELERATION,
-                        COMFORTABLE_DECELERATION)
+    TRUCK_PARAMETERS = VehicleParameters(
+        length=19,
+        desired_velocity=80 / 3.6,
+        desired_time_headway=1.7,
+        max_acceleration=0.3,
+        comfortable_deceleration=2.0
+    )
 
     def __init__(self, position, velocity, traffic_model, next_vehicle=None):
         super().__init__(position, velocity, traffic_model, self.TRUCK_PARAMETERS, next_vehicle)
+
+
+class AggressiveCar(Vehicle):
+    # Higher desired velocity, max_acceleration, comfortable deceleration and lower desired time headway
+    AGGRESSIVE_CAR_PARAMETERS = VehicleParameters(
+        length=5,
+        desired_velocity=130 / 3.6,
+        desired_time_headway=0.5,
+        max_acceleration=2.0,
+        comfortable_deceleration=5.0
+    )
+
+    def __init__(self, position, velocity, traffic_model, next_vehicle=None):
+        super().__init__(position, velocity, traffic_model, self.AGGRESSIVE_CAR_PARAMETERS, next_vehicle)
+
+
+class CarefulCar(Vehicle):
+    # Higher desired time gap and lower comfortable deceleration
+    CAREFUL_CAR_PARAMETERS = VehicleParameters(
+        length=5,
+        desired_velocity=100 / 3.6,
+        desired_time_headway=6,
+        max_acceleration=0.3,
+        comfortable_deceleration=2.0
+    )
+
+    def __init__(self, position, velocity, traffic_model, next_vehicle=None):
+        super().__init__(position, velocity, traffic_model, self.CAREFUL_CAR_PARAMETERS, next_vehicle)
